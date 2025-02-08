2d0ab70b1dec455365aab86dc29b254f582d58c1
Harsha184781 <sriharshabattu@gmail.com>
2025-02-08 10:58:11 +0530

    updating the project

--- cart.js
+++ cart.js
@@ -6,20 +6,20 @@ product.addEventListener("click",(e)=>{
 })
 
 
-var cartele=""
+
 let count = 0;
 let sum = 0 ;
 let cart = {};
 
 if (localStorage.getItem("count")){
-      count = parseInt(localStorage.getItem("count"));
+  count = parseInt(localStorage.getItem("count"));
 }
 if (localStorage.getItem("sum")){
-  sum = parseFloat(localStorage.getItem("sum"));
+sum = parseFloat(localStorage.getItem("sum"));
 }
 if (localStorage.getItem("cart")){
   cart = JSON.parse(localStorage.getItem("cart"));
-}
+  }
 
 updateCart();
 
@@ -35,7 +35,7 @@ async function data1(){
         data.push(ele);
     });
 
- carts();
+ carts(source);
 
 } 
 
@@ -46,35 +46,12 @@ data1();
 
 
 
-  function cartmatter(elementid,quantity,data){
+  // function cartmatter(elementid,quantity,data){
 
 
-   let productdata = data.find(ele=>ele.id== elementid );
-  
-   let image= productdata.image;
-   let title = productdata.title;
-   let price = productdata.price; 
-  
    
-     cartele += ` <div style="display:flex;  border:1px solid lightgrey">
 
-        <img style="height:120px; width:120px; padding:10px; margin-right:60px;" src="${image}"/>
-        <p style="width:400px;">${title}</p>
-        <div style="display:flex; flex-direction:column; margin-right:20px;">
-        <div style="display:flex;">
-         <button class="decrement" data-id=${elementid}  style="border:0px; background-color:white; padding:0px 20px; font-size:25px; position:relative; height:10px;  margin-right:30px; ">_</button>
-         <span class="numberchange" style="padding-top:10px;">${quantity}</span>
-         <button class="increment" data-id=${elementid} style="margin-left:30px; position:relative; top:0px; border:0px;  background-color:white;  padding:10px 20px; font-size:25px;">+</button>
-        </div>
-        <div>
-          <p style="text-align: center;">${quantity} x $${price}</p>
-        </div>
-      </div>
-    </div> 
-     
-    `
-
-  }
+  // }
 
 
 
@@ -87,41 +64,59 @@ data1();
     
   }
 
-function increment(){
+function increment(cart){
   let increbtns = document.querySelectorAll(".increment")
   
   
   increbtns.forEach((ele)=>{
-    console.log(ele);
+  
        ele.addEventListener("click",(e)=>{
             let cartid = e.target.dataset.id;
             let number =  cart[cartid].qty++;  
-            let quantityEle = document.querySelector(".numberchange")
-            quantityEle.innerHTML=number
-            //  if (e.target.classList.closest("numberchange")){
-              
-            //  }
-            // e.target.closest(".numberchange").innerHTML=number;
              localStorage.setItem("cart", JSON.stringify(cart));  
-             updateCart();
-
+            
+             carts(data);
             
             })
   })
-
-}
-
-
-function decrement(){
-  let decrebtns = document.querySelector(".decrement")
   
 }
 
 
+function decrement(cart){
+  let decrebtns = document.querySelectorAll(".decrement")
+
+  decrebtns.forEach((ele)=>{
+
+       ele.addEventListener("click",(e)=>{
+        
+            let cartid = e.target.dataset.id;
+            cart[cartid].qty--;
+           
+             localStorage.setItem("cart", JSON.stringify(cart));  
+             
+             carts(data);
+             
+            })
+  })
+ 
+  // cart = JSON.parse(localStorage.getItem("cart"));
+  // for(let ele in cart){
+  //   if(cart[ele].qty==0){
+
+  //   }
+
+  // }
+  }
 
 
 
-function carts(){
+
+
+function carts(data){
+ 
+    cart = JSON.parse(localStorage.getItem("cart"));
+    
   if(!localStorage.getItem("cart")){
       display=alert("Cart is Empty");
       let cartempty=` <div style="text-align:center;">
@@ -131,31 +126,66 @@ function carts(){
       defaultdiv.innerHTML=cartempty;
   }
   else{
-  
+
+   var cartele=""
    for(let ele in cart){
-      
+      if(cart[ele].qty!==0){
   
       let elementid = ele
       let quantity = cart[ele].qty
-      cartmatter(elementid,quantity,data);
+
+      let productdata = data.find(ele=>ele.id== elementid );
+  
+   let image= productdata.image;
+   let title = productdata.title;
+   let price = productdata.price; 
+  
+   
+     cartele += ` <div style="display:flex;  border:1px solid lightgrey">
+
+        <img style="height:120px; width:120px; padding:10px; margin-right:60px;" src="${image}"/>
+        <p style="width:400px; margin-top:30px; font-weight:bold;">${title}</p>
+        <div style="display:flex; flex-direction:column; margin-right:20px;">
+        <div style="display:flex;">
+         <button class="decrement" data-id=${elementid}  style="border:0px; background-color:white; padding:0px 20px; font-size:25px; position:relative; height:10px;  margin-right:30px; ">_</button>
+         <span class="numberchange" style="padding-top:10px;">${quantity}</span>
+         <button class="increment" data-id=${elementid} style="margin-left:30px; position:relative; top:0px; border:0px;  background-color:white;  padding:10px 20px; font-size:25px;">+</button>
+        </div>
+        <div>
+          <p style="text-align: center;">${quantity} <span style="font-weight:bold;">x $${price}<span/></p>
+        </div>
+      </div>
+    </div> 
+    
+    `
+  }
      
-   }   
-       
+   }
+
+  // let totalprice=0
+  //  for(let ele in cart){
+  //   console.log(cart[ele].qty);
+    
+  //     totalprice +=(cart[ele].qty*cart[ele].price);
+  // } 
+  let totalprice =  orderSummary(cart);
+ 
+
    defaultdiv.innerHTML=`<div style="display:flex;">
    <div style="margin:0px 0px 0px 200px; width:750px;">
     <div style="border:1px solid lightgrey; background-color:rgba(234,234,234,1); border-top-left-radius:5px; border-top-right-radius:5px;">
-      <p style="margin:10px;">Item List</p>
+      <p style="margin:10px; font-weight:bold;">Item List</p>
   </div>
  ${cartele}
   </div>
   <div style="margin:0px 0px 0px 50px; width:320px;">
       <div style="border:1px solid lightgrey; background-color:rgba(234,234,234,1);  border-top-left-radius:5px; border-top-right-radius:5px;">
-        <p style="margin:10px;">Order Summary</p>
+        <p style="margin:10px; font-weight:bold;">Order Summary</p>
       </div>
       <div style="padding:20px; border:1px solid lightgrey;">
         <div style="display:flex; justify-content: space-between;" >
         <p>Products()</p>
-        <p>$451</p>
+        <p>$${totalprice}</p>
       </div>
       <div style="display:flex; justify-content: space-between;">
         <p>Shipping</p>
@@ -163,14 +193,31 @@ function carts(){
       </div>
       <div style="display:flex; justify-content: space-between;">
         <p style="font-weight:bold;">Total amount</p>
-        <p>$451</p>
+        <p style= "font-weight:bold;">$${totalprice+30}</p>
       </div>
         <button  style="width:270px; padding:8px;" type="button" class="btn btn-dark">Go to checkout</button>
       </div>
     </div>
 </div>`
+increment(cart);
+decrement(cart);
 
-increment();
 
   }
 }
+
+
+
+function orderSummary(cart){
+ let totalprice = 0;
+for(let ele in cart){
+  console.log(cart[ele].qty);
+  
+    totalprice +=(cart[ele].qty*cart[ele].price);
+}
+return totalprice
+}
+
+
+
+
